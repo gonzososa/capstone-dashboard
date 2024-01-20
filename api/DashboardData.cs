011b6a2487cd2937c74c29de1ae8e71eedd29916
Gonzalo Sosa <gonzasosa@outlook.com>
2024-01-20 04:30:44 -0600

    getting headers

--- api/DashboardData.cs
+++ api/DashboardData.cs
@@ -7,6 +7,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Extensions.Primitives;
+using System.Collections.Generic;
 
 namespace api
 {
@@ -17,19 +19,29 @@ namespace api
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            /*log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            name = name ?? data?.name;*/
+            
+            List<string> headers = new List<string>();
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            foreach (var key in req.Headers.Keys) {
+                if (req.Headers.TryGetValue (key, out StringValues val)) {
+                    headers.Add (val.ToString());
+                } 
+            }
 
-            return new OkObjectResult(responseMessage);
+            var resp = JsonConvert.SerializeObject(headers);
+
+            //string responseMessage = string.IsNullOrEmpty(name)
+            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+            //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(resp);
         }
     }
 }
