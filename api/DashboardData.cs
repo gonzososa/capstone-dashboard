9f76aade1de885f3c897fd99f42ec9b477c30eb0
Gonzalo Sosa <gonzasosa@outlook.com>
2024-01-20 05:00:02 -0600

    route wildcard

--- api/DashboardData.cs
+++ api/DashboardData.cs
@@ -18,15 +18,7 @@ namespace api
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
-        {
-            /*log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;*/
-            
+        {            
             Dictionary<string, string> headers = new Dictionary<string, string>();
 
             foreach (var key in req.Headers.Keys) {
@@ -36,10 +28,6 @@ namespace api
             }
 
             var resp = JsonConvert.SerializeObject(headers);
-
-            //string responseMessage = string.IsNullOrEmpty(name)
-            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(resp);
         }
