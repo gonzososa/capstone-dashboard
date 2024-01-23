ecf9dd3f0fff33d80a681fc2d99719826010dd45
Gonzalo Sosa <gonzasosa@outlook.com>
2024-01-23 13:32:08 -0600

    Charting by average working

--- dashboard/src/store/data.js
+++ dashboard/src/store/data.js
@@ -6,3 +6,9 @@ export const getAllData = async () => {
     const response = await axios.get (`${API}/GetAllData`);
     return parseList (response);
 }
+
+export const getDataByDates = async (startDate, endDate, summarize) => {
+    let endpoint = `${API}/GetDataByDates?startDate=${startDate}&endDate=${endDate}&summarize=${summarize}`;
+    const response = await axios.get (endpoint)
+    return parseList (response);
+}
